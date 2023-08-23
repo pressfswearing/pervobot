0279e1dc75b0c17cbda57ba9779734c0adff0fda
Дмитрий Чучин <chuuchin@yandex.ru>
2023-08-23 13:19:13 +0300

    added username in id file

--- pervobot.py
+++ pervobot.py
@@ -45,7 +45,7 @@ def register1(message):
 	general_file = open('general_data.txt', 'a+', encoding='utf-8')
 	ids_file = open('ids.txt', 'a+', encoding='utf-8')
 	general_file.write('\n' + str(profile_join) + '@' + photo.file_id + '@' + message.from_user.username)
-	ids_file.write('\n' + str(message.from_user.id))
+	ids_file.write('\n' + str(message.from_user.id) + '@' + message.from_user.username)
 	bot.send_message(message.chat.id, 'Теперь ты зарегистрирован! Запусти /show_profile, чтобы увидеть свой профиль!')
 
 @bot.message_handler(commands=['login'])
